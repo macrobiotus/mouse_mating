62bb1a5917dfdaec77ebfaebdea9d95fe00553ea
Paul Czechowski <paul.czechowski@gmail.com>
2024-04-18 17:11:01 +0200

    working in script `015_r_use_saemix.R`   * started plotting out results better - got to RQ5   * (continue with plotting out model predictions)

--- 015_r_use_saemix.R
+++ 015_r_use_saemix.R
@@ -313,7 +313,7 @@ p.val
 
 # Sex has an influence on the body weight, the upper assymptote is higher for males.
 
-# RQ3: Do Sex and litter size have an association with the total growth? ----
+# RQ3: Do Sex and litter size have an association with the total weight gain? ----
 
 # _1.) Define data with sex and litter size covariates ----
 
@@ -426,36 +426,66 @@ DecayModel.RQ4 <- saemixModel(model = decay.model,
 
 DecayFIT.RQ4 <- saemix(DecayModel.RQ4, ModelData.RQ4, saemix.options)
 
-# _4.) Plot model ----
+# _4.) Inspect model ----
+
+DecayFIT.RQ4
+# Fixed effects CV(%) is mostly bad
+
+# Parameter          Estimate   SE    CV(%)  p-value 
+# A                  21.55263 0.9979    4.63 -       
+# beta_AnimalSex(A)   0.17613 0.0161    9.13 0.00e+00 *
+# beta_LitterSize(A)  0.02362 0.0099   41.89 1.70e-02 * 0.0170
+# beta_FatherDiet(A) -0.05120 0.0191   37.33 7.39e-03 * 0.00739
+# beta_MotherDiet(A) -0.00151 0.0180 1197.06 9.33e-01 
+# B                   3.24019 0.8274   25.54 -       
+# beta_AnimalSex(B)   0.10238 0.0692   67.62 1.39e-01
+# beta_LitterSize(B) -0.22072 0.0509   23.04 1.42e-05 * 0.0000142
+# beta_FatherDiet(B)  0.13391 0.0790   59.03 9.03e-02 
+# beta_MotherDiet(B) -0.29198 0.0794   27.20 2.36e-04 * 0.000236
+# k                   0.09233 0.0200   21.69 -       
+# beta_AnimalSex(k)   0.04126 0.0718  173.94 5.65e-01 
+# beta_LitterSize(k) -0.21285 0.0448   21.07 2.07e-06 *
+# beta_FatherDiet(k)  0.14874 0.0893   60.05 9.59e-02
+# beta_MotherDiet(k) -0.20287 0.0852   42.00 1.73e-02 * 
+# a.1                 0.77995 0.0402    5.15 -     
+
+# _5.) Plot model ----
 
 plot(DecayFIT.RQ4, plot.type="observations.vs.predictions" )
 plot(DecayFIT.RQ4, plot.type = "both.fit",  ilist = 1:9, smooth = TRUE)
 plot(DecayFIT.RQ4, plot.type="parameters.vs.covariates", ask=TRUE)
 npde.DecayFIT.RQ4 <- npdeSaemix(DecayFIT.RQ4) # skewness and kurtosis of normalised prediction discrepancies lower then in log model
 
-# _5.) Compare models ----
+# _6.) Compare models ----
 
 DecayFIT.RQ3
 DecayFIT.RQ4 # <- BIC AIC with diets worse then sex and litter size alone
 
 teststatRQ12 <- -2 * (as.numeric(logLik(DecayFIT.RQ3)) - as.numeric(logLik(DecayFIT.RQ4)))
 p.val <- pchisq(teststatRQ12, df = 3, lower.tail = FALSE)
-p.val # model with diets is not distinctly differnet from when not adding diets
+p.val # model with diets is not distinctly different from when not adding diets
 
+# _7.) Answer RQ4 ----
 
-# RQ5: What are the effects of diet within each sex and, and dependent on litter sizes? ----
+# Adding diet to the model with sex and litter size does not improve, but worsens it.
+# Adding diet to the model with sex and litter does not make it significantly different
+# from the respective null-model. The model is likely overfitted.
 
-# _1.) Define data with sex covariate ----
+# RQ5: What are the effects of diet within each sex individually dependent on litter sizes? ----
+
+# _1.) Split data by sexes ----
+
+# To avoid overparametrisations.
 
 ModelData.RQ5.male <- saemixData(
   name.data = {mice_f1_slct %>% filter(AnimalSex == "m")}, header = TRUE, name.group = c("AnimalId"), name.predictors = c("MeasurementDay"), name.response = c("BodyWeightG"), name.X = "MeasurementDay",
   name.covariates = c("LitterSize", "FatherDiet", "MotherDiet")
-)
+  )
 
 ModelData.RQ5.female <- saemixData(
   name.data = {mice_f1_slct %>% filter(AnimalSex == "f")}, header = TRUE, name.group = c("AnimalId"), name.predictors = c("MeasurementDay"), name.response = c("BodyWeightG"), name.X = "MeasurementDay",
   name.covariates = c("LitterSize", "FatherDiet", "MotherDiet")
-)
+  )
 
 # _2.) Define model object ----
 
@@ -469,26 +499,62 @@ DecayModel.RQ5 <- saemixModel(model = decay.model,
                               omega.init = matrix(c(1,0,0,0, 1,0,0,0,1),ncol=3, byrow=TRUE), 
                               error.model="constant")
 
-# _3.) Fit model ----
+# _3.) Fit models ----
 
 DecayFIT.RQ5.male <- saemix(DecayModel.RQ5, ModelData.RQ5.male, saemix.options)
 
 # Fixed effects
 # Parameter          Estimate    SE    CV(%)  p-value
 # A                  28.566939 2.5112    8.79 -      
-#   beta_LitterSize(A)  0.000838 0.0184 2199.03 0.9637 
-#   beta_FatherDiet(A) -0.035472 0.0285   80.24 0.2127 
-#   beta_MotherDiet(A) -0.053695 0.0285   53.04 0.0594 
+# beta_LitterSize(A)  0.000838 0.0184 2199.03 0.9637 
+# beta_FatherDiet(A) -0.035472 0.0285   80.24 0.2127 
+# beta_MotherDiet(A) -0.053695 0.0285   53.04 0.0594 *
 # B                   1.906307 0.7931   41.60 -      
-#   beta_LitterSize(B) -0.090955 0.0844   92.83 0.2814 
-#   beta_FatherDiet(B)  0.012596 0.1280 1016.57 0.9216 
-#   beta_MotherDiet(B)  0.059983 0.1354  225.69 0.6577 
+# beta_LitterSize(B) -0.090955 0.0844   92.83 0.2814 
+# beta_FatherDiet(B)  0.012596 0.1280 1016.57 0.9216 
+# beta_MotherDiet(B)  0.059983 0.1354  225.69 0.6577 
 # k                   0.045201 0.0160   35.44 -      
-#   beta_LitterSize(k) -0.059031 0.0739  125.14 0.4242 
-#   beta_FatherDiet(k)  0.060994 0.1139  186.66 0.5921 
-#   beta_MotherDiet(k)  0.173567 0.1191   68.64 0.1451 * 
-# a.1                 0.834962 0.0589    7.05 -      
+# beta_LitterSize(k) -0.059031 0.0739  125.14 0.4242 
+# beta_FatherDiet(k)  0.060994 0.1139  186.66 0.5921 
+# beta_MotherDiet(k)  0.173567 0.1191   68.64 0.1451 
+# a.1                 0.834962 0.0589    7.05 - 
 
+DecayFIT.RQ5.female <- saemix(DecayModel.RQ5, ModelData.RQ5.female, saemix.options)
+
+# Fixed effects
+# Parameter          Estimate   SE    CV(%) p-value
+# A                  22.41907 1.2216   5.45 -      
+# beta_LitterSize(A)  0.00652 0.0116 178.40 0.57510
+# beta_FatherDiet(A) -0.02591 0.0204  78.61 0.20333
+# beta_MotherDiet(A) -0.00363 0.0194 535.06 0.85174
+# B                   2.87427 0.9309  32.39 -      
+# beta_LitterSize(B) -0.13740 0.0688  50.09 0.04590
+# beta_FatherDiet(B) -0.03240 0.1159 357.75 0.77984
+# beta_MotherDiet(B) -0.29874 0.1095  36.65 0.00637 * but large variability across subjects
+# k                   0.07869 0.0207  26.27 -      
+# beta_LitterSize(k) -0.11206 0.0558  49.81 0.04469
+# beta_FatherDiet(k) -0.04751 0.1003 211.08 0.63567
+# beta_MotherDiet(k) -0.21824 0.0945  43.28 0.02086 * but large variability across subjects
+# a.1                 0.52514 0.0433   8.25 -      
+
+# _4.) Plot model fits ----
+
+# all looking good
+plot(DecayFIT.RQ5.female, plot.type="observations.vs.predictions" )
+plot(DecayFIT.RQ5.male, plot.type="observations.vs.predictions" )
+
+plot(DecayFIT.RQ5.female, plot.type = "both.fit",  ilist = 1:9, smooth = TRUE)
+plot(DecayFIT.RQ5.male, plot.type = "both.fit",  ilist = 1:9, smooth = TRUE)
+
+plot(DecayFIT.RQ5.female, plot.type="parameters.vs.covariates", ask=TRUE)
+plot(DecayFIT.RQ5.male, plot.type="parameters.vs.covariates", ask=TRUE)
+
+npde.DecayFIT.RQ4 <- npdeSaemix(DecayFIT.RQ5.female) # skewness and kurtosis of normalised prediction discrepancies lower then in log model
+npde.DecayFIT.RQ4 <- npdeSaemix(DecayFIT.RQ5.male) # skewness and kurtosis of normalised prediction discrepancies lower then in log model
+
+# _5.) Plot model predictions ----
+
+# **continue here** ----
 
 # testing male model
 
@@ -504,7 +570,7 @@ curve(
   xlab = "days [d]",
   ylab = "weight [g]",
   main = "male mice weight gain (C57BL6/NTac)"
-  )
+)
 
 # male mice growth - mother on HFD
 a <- a
@@ -517,24 +583,6 @@ curve(
   col = "red"
 )
 
-
-DecayFIT.RQ5.female <- saemix(DecayModel.RQ5, ModelData.RQ5.female, saemix.options)
-
-# Fixed effects
-# Parameter          Estimate   SE    CV(%) p-value
-# A                  22.41907 1.2216   5.45 -      
-#   beta_LitterSize(A)  0.00652 0.0116 178.40 0.57510
-#   beta_FatherDiet(A) -0.02591 0.0204  78.61 0.20333
-#   beta_MotherDiet(A) -0.00363 0.0194 535.06 0.85174
-# B                   2.87427 0.9309  32.39 -      
-#   beta_LitterSize(B) -0.13740 0.0688  50.09 0.04590 * 
-#   beta_FatherDiet(B) -0.03240 0.1159 357.75 0.77984
-#   beta_MotherDiet(B) -0.29874 0.1095  36.65 0.00637 * 
-# k                   0.07869 0.0207  26.27 -      
-#   beta_LitterSize(k) -0.11206 0.0558  49.81 0.04469
-#   beta_FatherDiet(k) -0.04751 0.1003 211.08 0.63567
-#   beta_MotherDiet(k) -0.21824 0.0945  43.28 0.02086 * 
-# a.1                 0.52514 0.0433   8.25 - 
 
 
 
@@ -576,23 +624,10 @@ curve(
 )
 
 
-# _4.) Plot model ----
 
-plot(DecayFIT.RQ5.female, plot.type="observations.vs.predictions" )
-plot(DecayFIT.RQ5.male, plot.type="observations.vs.predictions" )
+# _6.) Compare models ----
 
-plot(DecayFIT.RQ5.female, plot.type = "both.fit",  ilist = 1:9, smooth = TRUE)
-plot(DecayFIT.RQ5.male, plot.type = "both.fit",  ilist = 1:9, smooth = TRUE)
-
-plot(DecayFIT.RQ5.female, plot.type="parameters.vs.covariates", ask=TRUE)
-plot(DecayFIT.RQ5.male, plot.type="parameters.vs.covariates", ask=TRUE)
-
-npde.DecayFIT.RQ4 <- npdeSaemix(DecayFIT.RQ5.female) # skewness and kurtosis of normalised prediction discrepancies lower then in log model
-npde.DecayFIT.RQ4 <- npdeSaemix(DecayFIT.RQ5.male) # skewness and kurtosis of normalised prediction discrepancies lower then in log model
-
-# _5.) Compare models ----
-
-# no model for comparison built yet, needed would be above data sets without diets
+# no null model for comparison built yet, needed would be above data sets without diets
 
 # Snapshot environment ----
 
--- changelog.md
+++ changelog.md
@@ -809,7 +809,8 @@
     * finished code for all RQ4
     * diet not significant, unless sex is random effect, there only mothers hfd is significant
   * working in script `015_r_use_saemix.R`
-    * started plotting out results better - got to RQ4
+    * started plotting out results better - got to RQ5
+    * (continue with plotting out model predictions)
   
 
 ## Todo queue (last updated 16-04-2024)
